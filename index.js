bad1f3624ce92822aa4f00e0a58ecb37419e24a0
Argynbek Ysmail <argyyyn@gmail.com>
2024-02-10 19:52:10 +0600

    http/s request

--- index.js
+++ index.js
@@ -1,13 +1,7 @@
-const EventEmitter = require('events')
-const celebrity = new EventEmitter()
+const { get } = require('https')
 
-celebrity.on('post', title => {
-  if (title)
-    console.log('My celebrity posted a new post with title:', title)
+get('https://www.google.com', res => {
+  res.on('data', chunk => console.log(chunk))
+  res.on('end', _ => console.log('no more data'))
 })
 
-celebrity.emit('post', 'here is my haircut which i like')
-
-process.on('exit', code => {
-  console.log('process exit event with code', code)
-})
