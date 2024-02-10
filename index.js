15fb04ddf28bcac3f2b98ef9b3268e5ee8897a0c
Argynbek Ysmail <argyyyn@gmail.com>
2024-02-10 19:40:12 +0600

    event emitter

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
-console.log('argyyyn')
+const EventEmitter = require('events')
+const celebrity = new EventEmitter()
+
+celebrity.on('post', title => {
+  if (title)
+    console.log('My celebrity posted a new post with title:', title)
+})
+
+celebrity.emit('post', 'here is my haircut which i like')
+
+process.on('exit', code => {
+  console.log('process exit event with code', code)
+})
