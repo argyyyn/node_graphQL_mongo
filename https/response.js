f76ca246e257221941856965c7e06dfad3597d09
Argynbek Ysmail <argyyyn@gmail.com>
2024-02-12 12:17:36 +0600

    my first module

---
+++ https/response.js
@@ -0,0 +1,11 @@
+function decrypt (data) {
+  return 'decrypted data'
+}
+
+function read () {
+  return decrypt()
+}
+
+module.exports = {
+  read
+}
--- index.js
+++ index.js
@@ -4,4 +4,3 @@ get('https://www.google.com', res => {
   res.on('data', chunk => console.log(chunk))
   res.on('end', _ => console.log('no more data'))
 })
-
