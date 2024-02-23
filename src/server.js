025119587d04df9ab188ed4432b8467c64aaff6e
Argynbek <argyyyn@gmail.com>
2024-02-23 17:08:41 +0600

    same origin policy

--- src/server.js
+++ src/server.js
@@ -2,16 +2,40 @@ import http from "http";
 
 const PORT = 8000
 
-const server = http.createServer((req, res) => {
-  res.writeHead(200, {
-    'Content-type': 'application/json'
-  })
-  res.end(JSON.stringify({
-    name: 'Newton',
-    id: 123123
-  }))
-})
+const server = http.createServer()
 
 server.listen(PORT, _ => {
   console.log('listening for port', PORT)
+})
+
+
+server.on('request', (req, res) => {
+
+  const items = req.url.split('/')
+
+  if (items[1] === 'friends') {
+    res.statusCode = 200
+    res.setHeader('Content-type', 'application/json')
+
+    if (items.length === 3) {
+      res.end(JSON.stringify({
+        name: 'Newton',
+        id: items[2]
+      }))
+    }
+    else {
+      res.end(JSON.stringify(['friend 1', 'friend 2']))
+    }
+  }
+
+  else if (items[1] === 'messages') {
+    res.setHeader('Content-type', 'text/html')
+    res.write('<h1>argo</h1>')
+    res.end()
+  }
+
+  else {
+    res.statusCode = 404
+    res.end()
+  }
 })
