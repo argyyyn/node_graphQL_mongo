c49ae18edfe768b7e79a36bc0e7ceb68e4eb4bfa
Argynbek <argyyyn@gmail.com>
2024-03-18 16:40:26 +0600

    first express app route

--- src/index.js
+++ src/index.js
@@ -4,6 +4,11 @@ const app = express()
 
 const port = 8000
 
+const messages = [
+  {id: 1, name: 'argo'},
+  {id: 2, name: 'cargo'},
+]
+
 app.listen(port, () => {
 })
 
@@ -15,6 +20,14 @@ app.get('/messages', (req, res) => {
   res.send({id: 1, name: 'argo'})
 })
 
-app.post('/messages', (req, res) => {
+app.get('/messages/:id', (req, res) => {
+  const id = Number(req.params.id)
+  const message = messages[id]
 
+  if (message)
+    res.send(message)
+  else
+    res
+      .status(404)
+      .send({error: 'no message found'})
 })
